3530ef895c0f3d89cd64d9a5823d83df09ca7316
hkl-iii <rahleggon@gmail.com>
2021-06-09 05:44:24 +0900

    card component

---
+++ src/pages/card.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Card = props => {
+
+  const { card_id } = props;
+
+  useEffect(() => {
+
+  },[])
+
+  return (
+    <></>
+  )
+}
+
+export default Card;
