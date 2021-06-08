2793113bab2415c0cd8e98bfb2d5ad6bf6596490
hkl-iii <rahleggon@gmail.com>
2021-06-09 05:44:58 +0900

    flashcard component

---
+++ src/pages/flashcard.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Card from './card';
+
+const Flashcard = props => {
+
+  const [flashcards, setFlashcards] = useState([]);
+
+	useEffect(() => {
+		axios.get("http://localhost:8000/api/flashcard/").then((res) => {
+			setFlashcards(res.data);
+      console.log('f cards',res.data)
+		});
+	}, []);
+
+  return (
+    <>
+    {
+      flashcards.length > 0 && 
+      flashcards.map((item, idx) => (
+        <Card key={item.id} number={item.number} card_id={item.card}/>
+      ))
+    }
+    </>
+  )
+
+}
+
+export default Flashcard;
