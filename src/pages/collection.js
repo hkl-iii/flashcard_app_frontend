3b8ee2bac66b98bca686c6a8a430d7ded8300a7a
hkl-iii <rahleggon@gmail.com>
2021-06-09 10:53:42 +0900

    add edit form for cards list page

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useParams } from "react";
 import axios from "axios";
-import { Button } from "reactstrap";
+import { Button, Label, Col, Input } from "reactstrap";
 
 
 import Card from './card';
@@ -33,6 +33,27 @@ const Collection = props => {
           <Card key={item.id} number={item.number} card_id={item.card} />
         ))
       }
+      <form className="align-center w-60">
+				<h4>Edit Card</h4>
+				<div className="row">
+					<Label htmlFor="card_name" className="col-sm-4 col-form-Label">
+						Card name
+					</Label>
+					<Col sm={8}>
+						<Input
+							type="text"
+							className="form-control"
+							id="card_name"
+							required
+						/>
+					</Col>
+				</div>
+				<br />
+				<div className="row">
+					<Button color="danger" className="w-30 align-center" >Save</Button>
+				</div>
+			</form>
+      <br />
       <Button color="primary" onClick={() => props.history.push('/')}>
         Go to Back
       </Button>
