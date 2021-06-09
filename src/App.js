9ac5e7d85790ec49f3d365a9569a0a4f85f4a49a
hkl-iii <rahleggon@gmail.com>
2021-06-09 09:54:33 +0900

    setting routes for the project

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home";
 import Flashcard from "./pages/flashcard";
+import Collection from './pages/collection'
 
 function App() {
-
 	return (
 		<Router>
-			<div>
-				<nav>
-					<ul style={{}}>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/flashcard">FlashCard</Link>
-						</li>
-					</ul>
-				</nav>
-
-				{/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-				<Switch>
-					<Route path="/flashcard">
-						<Flashcard />
-					</Route>
-					<Route path="/">
-						<Home />
-					</Route>
-				</Switch>
-			</div>
+			<Switch>
+				<Route path="/collection/:id" render={(props) => <Collection {...props} /> } exact/>
+				<Route path="/" component={Home} />
+			</Switch>
 		</Router>
 	);
 }
