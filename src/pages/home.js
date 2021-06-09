5e0ab12bd74d23dc6eb6c70c1523443b5470c5b5
hkl-iii <rahleggon@gmail.com>
2021-06-09 12:06:50 +0900

    basic bootstrap table for collection

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { withRouter } from "react-router-dom";
 import { Button, Label, Col, Input } from "reactstrap";
+import BootstrapTable from 'react-bootstrap-table-next';
 
 const Home = (props) => {
 	const [collections, setCollections] = useState([]);
@@ -21,51 +22,65 @@ const Home = (props) => {
 		const card = {
 			name: name,
 		};
-		if(card.name) {
-			axios
-			.post(`http://localhost:8000/api/card/`, { ...card })
-			.then((res) => {
-				alert('Added new Card');
-				setName('');
+		if (card.name) {
+			axios.post(`http://localhost:8000/api/card/`, { ...card }).then((res) => {
+				alert("Added new Card");
+				setName("");
 			});
+		} else {
+			alert("Please type all fields");
 		}
-		else {
-			alert('Please type all fields');
-		}
-		
+	};
+
+	const columns = [
+		{
+			dataField: "id",
+			text: "Collection ID",
+		},
+		{
+			dataField: "name",
+			text: "Collection Name",
+		},
+		{
+			dataField: "Counts",
+			text: "Collection Counts",
+			formatter : (cellContent, row) => (
+				<span>{row.flashcard.length}</span>
+			)
+		},
+		{
+			dataField: "",
+			text: "Actions",
+			formatter : (cellContent, row) => (
+				<Button
+					color="primary"
+					onClick={() =>
+						gotoDetail(row.id, row.flashcard)
+					}
+				>
+					Collection Details
+				</Button>
+			)
+		},
+	];
+
+	const selectRow = {
+		mode: "checkbox",
+		clickToSelect: true,
+		style: { backgroundColor: '#c8e6c9' }
 	};
 
 	return (
 		<div className="align-center">
+			<br />
 			<h3>Collection Lists</h3>
-			<table className="align-center">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Counts</th>
-						<th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					{collections.length > 0 &&
-						collections.map((collection, idx) => (
-							<tr key={collection.id}>
-								<td>{collection.name}</td>
-								<td>{collection.flashcard.length}</td>
-								<td>
-									<Button
-										color="primary"
-										onClick={() =>
-											gotoDetail(collection.id, collection.flashcard)
-										}
-									>
-										Collection Details
-									</Button>
-								</td>
-							</tr>
-						))}
-				</tbody>
-			</table>
+			<BootstrapTable
+				bootstrap4
+				keyField="id"
+				data={collections}
+				columns={columns}
+				selectRow={selectRow}
+			/>
 			{collections.length == 0 && <h1>No Collection Data</h1>}
 			<br />
 			<hr />
@@ -88,7 +103,13 @@ const Home = (props) => {
 				</div>
 				<br />
 				<div className="row">
-					<Button color="danger" onClick={handleSubmit} className="w-30 align-center" >Save</Button>
+					<Button
+						color="danger"
+						onClick={handleSubmit}
+						className="w-30 align-center"
+					>
+						Save
+					</Button>
 				</div>
 			</form>
 		</div>
