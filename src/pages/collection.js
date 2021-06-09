b82a77f774fa79c65466161df92e05d334e4c6e9
hkl-iii <rahleggon@gmail.com>
2021-06-09 10:00:02 +0900

    collection detail page

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -1,16 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useParams } from "react";
 import axios from "axios";
+import { Button } from "reactstrap";
 
-import Flashcard from './flashcard';
+
+import Card from './card';
 
 const Collection = props => {
 
-  const { name, flashcard_id } = props;
-  
+  const cards = useState(props.location.state.data);
+  const [flashcards, setFlashcards] = useState([])
+  // console.log(props.match.params.id)
+
+  useEffect(async () => {
+    const temp= [];
+    for (const card of cards[0]) {
+      try {
+        const res = await axios.get("http://localhost:8000/api/flashcard/" + card);
+        temp.push(res.data)
+      }
+      catch(error) {
+        console.log(error);
+      }
+      setFlashcards([...temp]);
+    }
+  },[])
+
   return (
     <div className="align-center">
-      <h4>{name}</h4>
-      <br />
+      <h3>Card Lists</h3>
+      {
+        flashcards.map((item,idx) => (
+          <Card key={item.id} number={item.number} card_id={item.card} />
+        ))
+      }
+      <Button color="primary" onClick={() => props.history.push('/')}>
+        Go to Back
+      </Button>
     </div>
   )
 
